fbb99fa1c5552d74291b51b71e81a2275b866117
Gabriela <gab.jo.cuello@gmail.com>
2018-11-27 15:20:34 -0500

    Instructions

---
+++ research.rb
@@ -0,0 +1,10 @@
+# For each of the methods listed below
+#
+# Read the docs
+# Try out the method in irb using a few different values
+# Once you feel like you understand how it works, write down
+# its arguments (name, data type, optional/required, default value if any)
+# its return type
+# Write an example (in code) of how to use the method
+# run your code to make sure your example is valid!
+# The Ruby documentation can be alienating even for the most experienced developer. If (when) you get confused along the way, don't hesitate to ask for help! Also know that it's ok to not understand every word you read in the documentation. Your goal should be to piece together some understanding from the parts that do make sense.
